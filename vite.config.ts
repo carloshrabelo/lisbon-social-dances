fcf05547a166fd8e1d22f97ebcac30d370ef3e33
Carlos Henrique Rabelo de Oliveira <carlos.oliveira@complyadvantage.com>
2024-08-05 10:52:40 +0100

    GitLab Pages and GitLab CI

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/lisbon-social-dances/',
   plugins: [react()],
   server: {
     proxy: {
